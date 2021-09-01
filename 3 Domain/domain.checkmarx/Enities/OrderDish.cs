7fdc10729d465bae19b0658e303ec7e16cb8495c
IgorMatos <igormatos@gmail.com>
2021-09-02 00:48:57 +0100

    Fixes

---
+++ 3 Domain/domain.checkmarx/Enities/OrderDish.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace domain.checkmarx.Enities
+{
+    public class OrderDish
+    {
+        public Guid OrderId { get; set; }
+        public int DishId { get; set; }
+    }
+}
